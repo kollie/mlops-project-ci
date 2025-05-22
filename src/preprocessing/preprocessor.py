390a24ecdb9888f37468575d60aebd53008cd872
kollie <zikollie@gmail.com>
2025-05-22 13:00:26 +0200

    fixed validator error

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import yaml
