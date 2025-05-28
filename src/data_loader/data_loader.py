72e538363012320b4928ea6e9799bcd0336f99c8
hasalamanca <hasalamanca@student.ie.edu>
2025-05-28 10:45:01 +0200

    File from url, csv to pandas, cleaning of incomplete rows for no diagnosis, no race, deaths, no informed gender. All logged as info.

--- src/data_loader/data_loader.py
+++ src/data_loader/data_loader.py
@@ -22,6 +22,10 @@ class DataLoader:
     
     def _setup_logging(self):
         """Setup logging configuration."""
+        #Check path and directory if needed
+        log_file_path = self.config['logging']['file']
+        Path(log_file_path).parent.mkdir(parents=True, exist_ok=True)
+
         logging.basicConfig(
             level=self.config['logging']['level'],
             format=self.config['logging']['format'],
@@ -106,9 +110,58 @@ class DataLoader:
             self.logger.error(f"Error saving split data: {str(e)}")
             raise
 
+    def data_preparation(self, data: pd.DataFrame):
+        """
+        Data will be cleaned to make sure all ingested data is valid for the model.
+        It will follow a line by line approach and log the result with all the drops and fixes made
+
+        """
+
+        try:
+
+            initial_rows = len(data)
+
+            # Drop rows where there is no diagnosis, == '?'
+            diag_mask = (
+                (data['diag_1'] == '?') &
+                (data['diag_2'] == '?') &
+                (data['diag_3'] == '?')
+            )
+            dropped_diag = diag_mask.sum()
+            data = data[~diag_mask]
+            self.logger.info(f"Dropped {dropped_diag} rows without diagnoses (diag_1, diag_2, diag_3).")
+
+            # Drop rows without race, == '?'
+            dropped_race = (data['race'] == '?').sum()
+            data = data[data['race'] != '?']
+            self.logger.info(f"Dropped {dropped_race} rows with unknown race.")
+
+            # Drop rows where discharge_disposition_id == 11, which is death.
+            dropped_discharge = (data['discharge_disposition_id'] == 11).sum()
+            data = data[data['discharge_disposition_id'] != 11]
+            self.logger.info(f"Dropped {dropped_discharge} rows where patients expired (discharge_disposition_id == 11).")
+
+            # Drop rows where gender is 'Unknown/Invalid'
+            dropped_gender = (data['gender'] == 'Unknown/Invalid').sum()
+            data = data[data['gender'] != 'Unknown/Invalid']
+            self.logger.info(f"Dropped {dropped_gender} rows with unknown/invalid gender.")
+
+            # Total dropped rows
+            total_dropped = initial_rows - len(data)
+            self.logger.info(f"Total rows dropped during cleaning: {total_dropped}")
+
+            return data
+        except Exception as e:
+            self.logger.error(f"Error loading data: {str(e)}")
+            raise
+
 if __name__ == "__main__":
     # Example usage
     loader = DataLoader()
-    data = loader.load_data()
-    train, val, test = loader.split_data(data)
-    loader.save_split_data(train, val, test)
+    data = loader.load_data('https://drive.google.com/uc?id=1WCn9cjVQH2LP8E5ZC1-cNKVKBE8OrFGy')
+    data = loader.data_preparation(data)
+    
+    #train, val, test = loader.split_data(data)
+    #loader.save_split_data(train, val, test)
+
+    print(data.head())
